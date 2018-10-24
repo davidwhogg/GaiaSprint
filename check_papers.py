5035e39bedda4268ecc10efc489f060bf211573b
Andy Casey <andycasey@gmail.com>
2018-10-25 07:56:22 +1100

    minor

--- check_papers.py
+++ check_papers.py
@@ -1,6 +1,8 @@
 # pip install --upgrade google-api-python-client
 
 import os
+
+import ads
 from googleapiclient.discovery import build
 from subprocess import check_output
 
@@ -58,3 +60,67 @@ for each in missing:
     print("Could not find reference to {}".format(each))
 
 print("Fin.")
+
+
+match_acks = [
+    ("2018 NYC Gaia Sprint", "2018NYC.html"),
+    ("2017 Heidelberg Gaia Sprint", "2017HD.html"),
+    ("2016 NYC Gaia Sprint", "2016NYC.html")
+]
+
+
+# Search for each missing item.
+for each in missing:
+
+    arxiv_code = each.split("/")[-1]
+    results = list(ads.SearchQuery(q=f"identifier:\"{arxiv_code}\""))
+
+    # Find out which sprint it was.
+    assert len(results) > 0
+    for ack, page in match_acks:
+        r = list(ads.SearchQuery(q=f"identifier:\"{arxiv_code}\" AND ack:\"({ack})\""))
+        if len(r) > 0:
+            break
+
+    else:
+        page = None
+
+
+    for article in results:
+        title = article.title[0]
+        num_authors = len(article.author)
+        if num_authors == 1:
+            authors = article.author[0]
+        elif num_authors < 10:
+            authors = ", ".join(article.author[:-1]) + " and " + article.author[-1]
+        else:
+            authors = "{0} et al.".format(article.author[0])
+
+        new_content = f"    <li><a href=\"https://arxiv.org/abs/{arxiv_code}\">{title}</a>, {authors}</li>\n"
+
+        break
+
+    if page is None:
+        page = match_acks[0][-1]
+
+        print(f"Warning: auto-assigning to {page}:\n{new_content}")
+
+
+    print(f"PAGE: {page}")
+    print(new_content)
+
+    with open(page, "r") as fp:
+        # Find first <ul>
+        content = fp.read()
+
+    search_string = "<ul>\n"
+    pos = content.index(search_string) + len(search_string)
+
+    updated_content = content[:pos] + new_content + content[pos:]
+
+    with open(page, "w") as fp:
+        fp.write(updated_content)
+
+    os.system(f"git add {page}")
+    os.system(f"git commit -m \"auto-commit paper {arxiv_code}\"")
+
